6752eb053f5986a9e5001902f05fab09e0b9c948
Danuta Maliczak <danuta.m.maliczak@gmail.com>
2019-05-01 16:23:44 +0200

    initial project

---
+++ cin-cout/src/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+    string fullName;
+    string placeOfBirth;
+    string edu;
+    int inputYear;
+    int currentYear = 2019;
+ 
+    cout << "Enter your full name: ";
+    getline(cin, fullName);  //function for input string with whitespaces between words
+   
+    cout << "Enter full name of school you graduated from: " << endl;
+    getline(cin, edu);
+
+    cout <<"Enter place of birth: ";
+    getline(cin, placeOfBirth);
+
+    cout << "Enter year of birth: ";
+    cin >> inputYear;
+    int age = currentYear - inputYear;
+
+    std::cout <<"\n......................................." << std::endl;
+
+    cout << "\nName: " << fullName 
+         << "\nPlace of birth: " << placeOfBirth
+         << "\nYear of birth: " << inputYear 
+         << " (Age: " << age << ")" 
+         << "\nGraduated from: " << edu << endl;
+
+       return 0; 
+}
